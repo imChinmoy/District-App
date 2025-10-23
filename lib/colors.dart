653c2dd1f0554e4063179abc2b7091d7e68e8fc0
Pranav291205 <prvrt29@gmail.com>
2025-10-24 04:57:43 +0530

    icons added, seach bar ui improved

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,4 +5,8 @@ class AppColors {
   static final Color backgrondColor2 = Color(0xFF20164B);
   static final Color splashscreencolor = Color(0xFF381BA9);
   static final Color textcolor = Colors.white;
+  static final Color foryou = Color(0xFF6746C3);
+  static final Color dining = Color(0xFFFF4D6A);
+  static final Color events = Color(0xFF0D47A1);
+  static final Color movies = Color(0xFFFFC000);
 }
--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -1,32 +1,65 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:district/colors.dart';
 import 'package:district/features/home/location.dart';
 import 'package:district/features/home/searchpage.dart';
 import 'package:district/features/home/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class HomeScreen extends ConsumerWidget {
+class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends ConsumerState<HomeScreen> {
+  int _selectedIndex = 0;
+
+  final List<Map<String, dynamic>> _tabs = [
+    {
+      'icon': Icons.stars,
+      'label': 'FOR YOU',
+      'color': AppColors.foryou,
+    },
+    {
+      'icon': Icons.restaurant,
+      'label': 'DINING',
+      'color': AppColors.dining,
+    },
+    {
+      'icon': Icons.confirmation_number,
+      'label': 'EVENTS',
+      'color': AppColors.events,
+    },
+    {
+      'icon': Icons.movie,
+      'label': 'MOVIES',
+      'color': AppColors.movies,
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     final location = ref.watch(currentLocationProvider);
     
     return Scaffold(
-        backgroundColor:Colors.black,
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(69),
-          child: _buildCustomAppBar(context, ref, location),
-        ),
-        body: SingleChildScrollView(
-          child: Container(
-            child: Column(
-              children: [
-                _buildSearchBar(context),
-                // Additional content 
-              ],
-            ),
+      backgroundColor: Colors.black,
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(69),
+        child: _buildCustomAppBar(context, ref, location),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              _buildSearchBar(context),
+              _buildCategoryTabs(),
+              // Additional content 
+            ],
           ),
         ),
+      ),
     );
   }
   
@@ -42,16 +75,15 @@ class HomeScreen extends ConsumerWidget {
               },
               child: Container(
                 child: Icon(
-                        Icons.location_on_outlined,
-                        color: Colors.white70,
-                        size: 24,
-                      ),
+                  Icons.location_on_outlined,
+                  color: Colors.white70,
+                  size: 24,
+                ),
               ),
             ),
             
             const SizedBox(width: 12),
             
-
             Expanded(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -106,7 +138,7 @@ class HomeScreen extends ConsumerWidget {
                 width: 45,
                 height: 45,
                 decoration: BoxDecoration(
-                  color:Colors.white70,
+                  color: Colors.white70,
                   borderRadius: BorderRadius.circular(25),
                 ),
                 child: const Icon(
@@ -121,6 +153,7 @@ class HomeScreen extends ConsumerWidget {
       ),
     );
   }
+
   Widget _buildSearchBar(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
@@ -148,21 +181,104 @@ class HomeScreen extends ConsumerWidget {
             const SizedBox(width: 16),
             Icon(
               Icons.search,
-              color: Colors.white,
+              color: Colors.white70,
               size: 24,
             ),
             const SizedBox(width: 12),
-            Text(
-              "Search for 'Sonu Nigam'",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 16,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
+            AnimatedTextKit(
+              animatedTexts: [
+                RotateAnimatedText(
+                  "Search for 'Sonu Nigam'",
+                  textStyle: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                RotateAnimatedText(
+                  "Search for 'Arijit Singh'",
+                  textStyle: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                RotateAnimatedText(
+                  "Search for 'Shreya Ghoshal'",
+                  textStyle: TextStyle(
+                    color: Colors.white70,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+              ],
+              repeatForever: true,
+            )
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildCategoryTabs() {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: List.generate(
+          _tabs.length,
+          (index) => GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            child: _buildTabItem(
+              icon: _tabs[index]['icon'],
+              label: _tabs[index]['label'],
+              color: _tabs[index]['color'],
+              isSelected: _selectedIndex == index,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildTabItem({
+    required IconData icon,
+    required String label,
+    required Color color,
+    required bool isSelected,
+  }) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(
+          icon,
+          color: isSelected ? color : Colors.grey,
+          size: 28,
+        ),
+        const SizedBox(height: 4),
+        Text(
+          label,
+          style: TextStyle(
+            color: isSelected ? color : Colors.grey,
+            fontSize: 12,
+            fontWeight: isSelected ? FontWeight.bold : FontWeight.normal,
+          ),
+        ),
+        const SizedBox(height: 4),
+        AnimatedContainer(
+          duration: Duration(milliseconds: 200),
+          height: 3,
+          width: isSelected ? 40 : 0,
+          decoration: BoxDecoration(
+            color: color,
+            borderRadius: BorderRadius.circular(2),
+          ),
+        ),
+      ],
     );
   }
 }
